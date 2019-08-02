af2f1fffa033427fbd1739931dd361085b667fec
William Whinn <w.willis.1986@gmail.com>
2019-08-02 11:45:28 +0100

    Update copyright notice

--- patina_user_youtube_dl.sh
+++ patina_user_youtube_dl.sh
@@ -5,7 +5,7 @@
 ##########
 
 # Patina User Components: Additional components to simplify common tasks.
-# Copyright (C) 2018 William Willis Whinn
+# Copyright (C) 2019 William Willis Whinn
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
